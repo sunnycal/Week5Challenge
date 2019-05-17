f5b53730dfc3211a21d81b7d34e79f8405f7a88c
sunnycal <milana.campbell@gmail.com>
2019-05-17 14:28:15 -0400

    first commit

---
+++ src/main/java/com/example/demo/MessageRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message,
+Long>{
+}
+
---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,84 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Entity
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=4)
+    private String title;
+
+    @NotNull@Size
+    @Size(min=3)
+    private String content;
+
+    @NotNull
+    @Size(min=10)
+    private int postedDate;
+
+    @NotNull
+    @Min(3)
+    private String postedBy;
+
+    @OneToMany(mappedBy = "User", cascade = CascadeType.ALL,
+    fetch = FetchType.EAGER)
+    public Set<Message> messages;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(int postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public String getPostedBy() {
+        return postedBy;
+    }
+
+    public void setPostedBy(String postedBy) {
+        this.postedBy = postedBy;
+    }
+
+    public Set<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(Set<Message> messages) {
+        this.messages = messages;
+    }
+}
